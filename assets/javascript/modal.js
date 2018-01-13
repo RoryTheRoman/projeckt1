eada6e3d6d82766f0027ab4ac7b213dba956b401
Mike D <michael.mcdaniel210@gmail.com>
2018-01-13 12:35:05 -0600

    added moment.js link and working with date input format

--- assets/javascript/modal.js
+++ assets/javascript/modal.js
@@ -1,9 +1,29 @@
+//disables clicking outside of modal to remove it:
+//this must remain outside of document.ready 
+$('#ageModal').modal({
+    backdrop: 'static',
+    keyboard: false
+});
+
 $(document).ready(function(){
-	$("#ageModal").modal('show');
+	// $("#ageModal").modal('show');
+	$("#ageSubmitButton").on("click", function(){
+		var userDate = $("#ageInput").val();
+		console.log("userDate" + userDate);				
+		var randomFormat = "YYYY/MM/DD";
+	    var convertedDate = moment(userDate, randomFormat);
+	    console.log("conv" + convertedDate);
+    console.log(moment(convertedDate).toNow());
+    console.log(moment(convertedDate).diff(moment(), "years"));
+	    
+
+	});
 
 
-	$('#ageModal').modal({
-	  backdrop: 'static',
-	  keyboard: false
-	})
+
+
+
+
+
+
 });//closes document.ready function
